9c8c97cc796e98cf84ee0f3d11280d63b5777c62
andreabrantes80 <meloabrantes@gmail.com>
2026-04-05 21:15:34 -0300

    Ajuste Prisma 7 e tipos do Node

---
+++ prisma.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  datasource: {
+    // Agora o TypeScript vai entender o 'process.env'
+    url: process.env.DATABASE_URL as string,
+  },
+});
