35ec905c4c1f7c0efb4bada941d22df57b1084ff
andreabrantes80 <meloabrantes@gmail.com>
2026-04-07 22:11:48 -0300

    feat: implementado campo balance no create user

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -3,14 +3,15 @@ import { CreateUserService } from '../../services/user/CreateUserService'
 
 class CreateUserController{
   async handle(request: Request, response:Response){
-    const {name, email, password} = request.body;
+    const {name, email, password, balance} = request.body;
 
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({
       name,
       email,
-      password
+      password,
+      balance: balance !== undefined ? balance : 0
     })
 
     return response.json(user);
