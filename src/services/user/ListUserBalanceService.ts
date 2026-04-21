2b10f6cdad710c6baae174d14eaa6a0eb01ba3c1
andreabrantes80 <meloabrantes@gmail.com>
2026-04-20 22:57:06 -0300

    ajustes no balance servcie

--- src/services/user/ListUserBalanceService.ts
+++ src/services/user/ListUserBalanceService.ts
@@ -1,99 +1,3 @@
-// import { prismaClient } from "../../prisma";
-
-// interface UserRequest {
-//   user_id: string;
-//   date: string;
-// }
-
-// interface ItemProp{
-//   id: string;
-//   description: string;
-//   value: number;
-//   type: string;
-//   date: Date;
-//   user_id: string;
-// }
-
-// class ListUserBalanceService{
-//   async execute({ user_id, date }: UserRequest){
-
-//     if (!user_id) {
-//       throw new Error("Invalid user");
-//     }
-
-//     const dashboard = [];
-//     const findUser = await prismaClient.user.findFirst({
-//       where:{
-//         id: user_id,
-//       }
-//     })
-
-//     if (!findUser) {
-//       throw new Error("User not found");
-//     }
-
-//     const data = {
-//       tag: 'saldo',
-//       saldo: findUser.balance
-//     }
-
-//     const findReceive = await prismaClient.receive.findMany({
-//       where:{
-//         date: date,
-//         user_id: user_id,
-//         type: 'receita'
-//       }
-//     })
-//     console.log(findReceive);
-
-//     const findExpenses = await prismaClient.receive.findMany({
-//       where:{
-//         date: date,
-//         user_id: user_id,
-//         type: 'despesa'
-//       }
-//     })
-//     console.log(findExpenses);
-
-
-//   //  const resultRevenue = findReceive.reduce(getSoma, 0);
-
-//   //  const resultExpenses = findExpenses.reduce(getSoma, 0);
-
-//   //   function getSoma(total: number, num: ItemProp) {
-//   //     return total + num.value;
-//   //   }
-
-//     const resultRevenue = findReceive.reduce((total, item) => {
-//   return total + item.value;
-//     }, 0);
-//     console.log(resultRevenue);
-
-// const resultExpenses = findExpenses.reduce((total, item) => {
-//   return total + item.value;
-// }, 0);
-//     console.log(resultExpenses);
-
-//     const sumDailyRevenue = {
-//       tag: 'receita',
-//       saldo: resultRevenue
-//     }
-
-//     const sumDailyExpense = {
-//       tag: 'despesa',
-//       saldo: resultExpenses
-//     }
-
-//     dashboard.push(data, sumDailyRevenue, sumDailyExpense);
-
-//     return dashboard;
-//   }
-
-// }
-
-
-// export { ListUserBalanceService }
-
 import { prismaClient } from "../../prisma";
 
 interface UserRequest {
@@ -110,33 +14,25 @@ class ListUserBalanceService {
 
     const dashboard = [];
 
-    const findUser = await prismaClient.user.findFirst({
-      where: {
-        id: user_id,
-      }
+    const findUser = await prismaClient.user.findUnique({
+      where: { id: user_id }
     });
 
     if (!findUser) {
       throw new Error("User not found");
     }
 
-    // 🔥 saldo total do usuário
-    const data = {
-      tag: 'saldo',
-      saldo: findUser.balance
-    };
-
-    // 🔥 corrigindo filtro de data (intervalo do dia)
+    // 🔹 intervalo do dia
     const startOfDay = new Date(date);
     startOfDay.setHours(0, 0, 0, 0);
 
     const endOfDay = new Date(date);
     endOfDay.setHours(23, 59, 59, 999);
 
-    // 🚀 apenas 1 query (melhor performance)
-    const allMovements = await prismaClient.receive.findMany({
+    // 🔹 buscar movimentações
+    const movements = await prismaClient.receive.findMany({
       where: {
-        user_id: user_id,
+        user_id,
         date: {
           gte: startOfDay,
           lte: endOfDay
@@ -144,31 +40,31 @@ class ListUserBalanceService {
       }
     });
 
-    // 🔥 soma receitas
-    const resultRevenue = allMovements
-      .filter(item => item.type === 'receita')
-      .reduce((total, item) => {
-        return total + item.value;
-      }, 0);
+    // 🔹 somar depósitos
+    const totalDeposit = movements
+      .filter(item => item.type === 'deposit')
+      .reduce((acc, item) => acc + item.value, 0);
 
-    // 🔥 soma despesas
-    const resultExpenses = allMovements
-      .filter(item => item.type === 'despesa')
-      .reduce((total, item) => {
-        return total + item.value;
-      }, 0);
+    // 🔹 somar saídas
+    const totalWithdraw = movements
+      .filter(item => item.type === 'withdraw')
+      .reduce((acc, item) => acc + item.value, 0);
 
-    const sumDailyRevenue = {
-      tag: 'receita',
-      saldo: resultRevenue
-    };
-
-    const sumDailyExpense = {
-      tag: 'despesa',
-      saldo: resultExpenses
-    };
-
-    dashboard.push(data, sumDailyRevenue, sumDailyExpense);
+    // 🔥 retorno padronizado
+    dashboard.push(
+      {
+        tag: 'saldo',
+        amount: findUser.balance
+      },
+      {
+        tag: 'deposit',
+        amount: totalDeposit
+      },
+      {
+        tag: 'withdraw',
+        amount: totalWithdraw
+      }
+    );
 
     return dashboard;
   }
