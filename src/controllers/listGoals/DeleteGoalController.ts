4edcdf6e223229fac24c1e7ca53417646216dbd6
andreabrantes80 <meloabrantes@gmail.com>
2026-04-21 23:12:26 -0300

    debug delete goal logs

--- src/controllers/listGoals/DeleteGoalController.ts
+++ src/controllers/listGoals/DeleteGoalController.ts
@@ -3,6 +3,12 @@ import { DeleteGoalService } from "../../services/listGoals/DeleteGoalService";
 
 class DeleteGoalController {
   async handle(req: Request, res: Response) {
+
+        console.log("PARAMS:", req.params);
+    console.log("ID:", req.params.id);
+    console.log("USER:", req.user_id);
+
+
     const goal_id = req.params.goal_id as string;
     const user_id = req.user_id;
 
